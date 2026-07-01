b46ef1137c2e08279fa1a34f19f7020c8d112ed5
byFranca <miguel.sou.franca@gmail.com>
2026-07-01 01:14:11 -0300

    Adicionando verificação de cor e reestruturando o codigo

--- botFnaf.py
+++ botFnaf.py
@@ -1,8 +1,14 @@
 import pyautogui as pg
 import time
 from pynput import keyboard
+
+pg.PAUSE = 0.05
+
 pausa = 0.02
 rodando = True
+
+portaEsquerdaFechada = False
+portaDireitaFechada = False
 
 coordenadas = {
     "luzDireita": (0.95546875, 0.6402777777777777),
@@ -11,82 +17,87 @@ coordenadas = {
     "portaEsquerda": (0.04453125, 0.4625),
     "cameraAcao": (0.43072916666666666, 0.98),
     "cameraReset": (0.43072916666666666, 0.85),
-    "chicaCheckPorta" : (0.6697916666666667, 0.5333333333333333),
-    "bonnieCheckSeContinuaLa1" : (0.38177083333333334, 0.40185185185185185),
-    "bonnieCheckSeContinuaLa2" : (0.38229166666666664, 0.4666666666666667),
-    "bonnieCheckPorta" : (0.12604166666666666, 0.3574074074074074),
-    "cameraFreddy": (0.84609375, 0.8944444444444445)
+    "cameraFreddy": (0.84609375, 0.8944444444444445),
+    "bonnieCheckPorta": (0.12604166666666666, 0.3574074074074074),
+    "chicaCheckPorta": (0.6697916666666667, 0.5333333333333333)
 }
 
 def moverMouse(posicao):
-    width, heigth = pg.size()
-    pg.moveTo(posicao[0]*width, posicao[1]*heigth)
-    
+    width, height = pg.size()
+    pg.moveTo(posicao[0] * width, posicao[1] * height)
 
 def clicar(posicao):
     moverMouse(posicao)
-    time.sleep(0.3)
     pg.mouseDown()
     time.sleep(pausa)
     pg.mouseUp()
-    
-def pegarcoord():
-    width, heigth = pg.size()
-    mx, my = pg.position()
-    posicaoX = mx/width
-    posicaoY = my/heigth
-    coord = posicaoX, posicaoY
-    return coord
-   
-def pegarCor(coord):
-    ft = pg.screenshot()
-    width, height = ft.size
-    x = int(coord[0] * width)
-    y = int(coord[1] * height)
-    pixel = ft.getpixel((x, y))
-    return pixel 
-
-def checarEsquerda():
-    clicar(coordenadas["luzEsquerda"])
-    time.sleep(0.01)
-    clicar(coordenadas["luzEsquerda"])
-
-def checarDireita():
-    clicar(coordenadas["luzDireita"])
-    time.sleep(0.01)
-    clicar(coordenadas["luzDireita"])
 
 def alternarCamera():
     moverMouse(coordenadas["cameraAcao"])
     time.sleep(0.1)
     moverMouse(coordenadas["cameraReset"])
 
+def clicarCamera(nomeCamera):
+    clicar(coordenadas[nomeCamera])
+
 def abrirFecharCameraRapido():
     alternarCamera()
     time.sleep(0.3)
     alternarCamera()
 
-def checarFreddy():
-    alternarCamera()
-    time.sleep(0.3)
-    clicar(coordenadas["cameraFreddy"])
-    time.sleep(0.1)
-    alternarCamera()
+def apertarBotao(nomeBotao):
+    clicar(coordenadas[nomeBotao])
+
+def checarLuzEsquerda():
+    apertarBotao("luzEsquerda")
+    time.sleep(0.15)
+    apertarBotao("luzEsquerda")
+
+def checarLuzDireita():
+    apertarBotao("luzDireita")
+    time.sleep(0.15)
+    apertarBotao("luzDireita")
+
+def alternarPortaEsquerda():
+    apertarBotao("portaEsquerda")
+
+
+def alternarPortaDireita():
+    apertarBotao("portaDireita")
+
+def pegarCor(posicao):
+    foto = pg.screenshot()
+    width, height = foto.size
+    x = int(posicao[0] * width)
+    y = int(posicao[1] * height)
+    cor = foto.getpixel((x, y))
+    return cor
+
+def corParecida(corAtual, corEsperada, tolerancia):
+    return (
+        abs(corAtual[0] - corEsperada[0]) <= tolerancia and
+        abs(corAtual[1] - corEsperada[1]) <= tolerancia and
+        abs(corAtual[2] - corEsperada[2]) <= tolerancia
+    )
 
 def pararBot(tecla):
     global rodando
-
     try:
         if tecla.char == "q":
             rodando = False
             print("Parando bot...")
-    except AttributeError:
+            return False
+    except:
         pass
+escutarTecla = keyboard.Listener(on_press=pararBot)
+escutarTecla.start()
 
-escutador = keyboard.Listener(on_press=pararBot)
-escutador.start()
 
+# Parte de execução
+time.sleep(4)
 while rodando:
-    time.sleep(0.5)
-    cord = pegarcoord()
-    print(pegarCor(cord))
+    checarLuzDireita()
+    cor = pegarCor(coordenadas["chicaCheckPorta"])
+    if corParecida(cor, (86, 95, 9), 20):
+        alternarPortaDireita()
+        rodando = False
